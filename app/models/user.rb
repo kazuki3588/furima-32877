bb8a9e6a88846a9b66bcf2a1ed6cab71692ad29a
kazuki3588 <kuramoto880@ezweb.ne.jp>
2021-01-17 09:29:30 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :last_name
+    validates :first_name
+    validates :last_furigana
+    validates :first_furigana
+  end
+  validates :date, presence: true
+  
+end
