392def0035752a9de001f42a60d63c22ec2f429f
kazuki3588 <kuramoto880@ezweb.ne.jp>
2021-01-29 08:08:32 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-  before_action :move_set, only: [:show, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_set, only: [:show, :destroy, :edit, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -24,6 +24,23 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
+  
+  def edit
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :fee_id, :price, :shipping_area_id, :delivery_date_id, :image).merge(user_id: current_user.id)
